1a4cd100e489a93275d41bcd41ef1a98653946fb
unknown <marcilioleite@live.com>
2013-06-09 21:20:33 -0300

    Timer adicionado

--- core/events.js
+++ core/events.js
@@ -1,9 +1,11 @@
 function pause() {
 	gameinfo.paused = true
 	window.clearInterval(gameloop)
+	window.clearInterval(timer)
 }
 
 function resume() {
 	gameinfo.paused = false
 	gameloop = window.setInterval(loop, 1000 / gameinfo.fps)
+	timer = window.setInterval(startTimer, 1000)
 }
--- core/game.js
+++ core/game.js
@@ -3,13 +3,20 @@ var graphics = canvas.getContext("2d")
 
 var gameinfo = {
 	fps: 2,
-	paused: false
+	paused: false,
+	elapsedTime: 0,
+	meScore: 0,
+	opponentScore: 0
 }
 
 function update() {
 }
 
 function draw() {
+}
+
+function startTimer() {
+	gameinfo.elapsedTime++
 }
 
 function loop() {

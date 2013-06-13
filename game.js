f4ff05b7339e6cd3fdb45b295a1e2849e8ad8e50
unknown <marcilioleite@live.com>
2013-06-13 12:25:07 -0300

    Relogio adicionado

--- game.js
+++ game.js
@@ -1,24 +1,64 @@
-var sprite = null
-
-function hello() {
-	console.log('hello')
+var gameData = {
+	time: 	0,
+	money: 	0
 }
 
-function MouseDemo () {
-    MouseDemo.superclass.constructor.apply(this, arguments)
+var gui = {
+	timeLabel:	null,
+	moneyLabel: null
+}
+
+var sprite = null
+
+function yield() {
+	gameData.money += 10
+	gui.moneyLabel.string = gameData.money.toString()
+}
+
+function ticTac() {
+	gameData.time++
+	gui.timeLabel.string = gameData.time.toString()
+}
+
+function PirateBay () {
+    PirateBay.superclass.constructor.apply(this, arguments)
     this.isMouseEnabled = true
     var s = cc.Director.sharedDirector.winSize
+
+    gui.timeLabel = new cc.Label({
+    	string: gameData.time.toString(),
+    	fontName: 'Londrina Solid',
+    	fontSize: 30 
+    })
+    gui.timeLabel.position = new cc.Point(50, 100)
+        
+    gui.moneyLabel = new cc.Label({
+    	string: gameData.money.toString(),
+    	fontName: 'Londrina Solid',
+    	fontSize: 30 
+    })
+    gui.moneyLabel.position = new cc.Point(100, 100)
     
-    sprite = new cc.Sprite({ url: 'grossini.png' })
+    sprite = new cc.Sprite({ 
+    	url: 'grossini.png'
+    })
     sprite.position = { x: 200, y: 200 }
-    sprite.speed = 100
+    sprite.speed = 101
     
-    this.schedule({method: hello, interval: 1})
-    
+    this.schedule({
+    	method: yield, 
+    	interval: 10
+    })
+    this.schedule({
+    	method: ticTac, 
+    	interval: 1
+    })
+    this.addChild(gui.timeLabel)
+	this.addChild(gui.moneyLabel) 
     this.addChild(sprite)
 }
 
-MouseDemo.inherit(cc.Layer, {
+PirateBay.inherit(cc.Layer, {
     mouseDown: function (event) {
     	start = sprite.position
     	target = event.locationInCanvas
@@ -32,10 +72,9 @@ MouseDemo.inherit(cc.Layer, {
 function runDemo () {
     var director = cc.Director.sharedDirector
     director.attachInView(document.getElementById('cocos2d-demo'))
-    director.displayFPS = false
-    
+    director.displayFPS = true
     var scene = new cc.Scene
-    scene.addChild(new MouseDemo)
+    scene.addChild(new PirateBay)
 
     director.runWithScene(scene)
 }
